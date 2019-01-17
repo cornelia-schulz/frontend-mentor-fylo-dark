06e2a31871d5bc1bf41421d4338401bf69c7a74b
Cornelia <37396830+cornelia-schulz@users.noreply.github.com>
2019-01-17 18:49:00 +1300

    Add files via upload

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,12 +1,15 @@
 import React, {useState} from 'react'
-// import intro from '../images/illustration-intro.png'
+import intro from '../images/illustration-intro.png'
 
 function Intro() {
     return (
-        <article>
-            {/* <img src={intro} alt='illustration of our services' /> */}
-            <h1></h1>
-        </article>
+        <section className='intro'>
+            <img src={intro} alt='illustration of our services' />
+            <h1>All your files in one secure location, accessible anywhere.</h1>
+            <p>Fylo stores all your most important files in one secure location. Access them wherever 
+  you need, share and collaborate with friends family, and co-workers.</p>
+            <button><span>Get Started</span></button>
+        </section>
     )
 }
 
